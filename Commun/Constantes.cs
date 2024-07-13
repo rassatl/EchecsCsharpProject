7f68ba6e0250c780ed5f97524b05ca253422fa73
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2024-07-14 01:22:59 +0200

    Déplacement des pièces fix

--- Commun/Constantes.cs
+++ Commun/Constantes.cs
@@ -8,7 +8,25 @@ namespace Commun
 {
     public static class Constantes
     {
+        // Taille de la fenêtre
         public const int WIDTH_MIN_SIZE = 640;
         public const int HEIGHT_MIN_SIZE = 480;
+
+        // Nom des pièces
+        public const string ROI_B = "RoiBlanc";
+        public const string REINE_B = "ReineBlanc";
+        public const string FOU_B = "FouBlanc";
+        public const string CAVALIER_B = "CavalierBlanc";
+        public const string TOUR_B = "TourBlanc";
+        public const string PION_B = "PionBlanc";
+
+        public const string ROI_N = "RoiNoir";
+        public const string REINE_N = "ReineNoir";
+        public const string FOU_N = "FouNoir";
+        public const string CAVALIER_N = "CavalierNoir";
+        public const string TOUR_N = "TourNoir";
+        public const string PION_N = "PionNoir";
+
+
     }
 }
